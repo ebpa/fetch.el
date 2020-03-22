796a02f79cedebbc5afbfa64b9cd060e07067c62
Erik Anderson <erikbpanderson@gmail.com>
2020-03-22 14:09:14 -0500

    Add missing package.el entries

--- fetch.el
+++ fetch.el
@@ -4,6 +4,8 @@
 
 ;; Author: Erik Anderson <erik@ebpa.link>
 ;; Keywords: tools
+;; Package-Requires: ((emacs "24.4"))
+;; Version: 0.0.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
